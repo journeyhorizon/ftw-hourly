6b1a1442a7cec143d0c1d820ff0df7fb4bf26707
tam.vu <vuthanhtam1506@gmail.com>
2022-07-15 09:48:13 +0700

    update code onlt for test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,8 +18,11 @@ docker login -u AWS -p $(aws ecr get-login-password --region ${AWS_INSTANCE_REGI
 echo -e "${COLOR}::::pushing to aws repo::::${NC}"
 docker push ${AWS_ECR_REPO_URL}
 
-echo -e "${COLOR}::::ssh and deploy::::${NC}"
-ssh -o StrictHostKeyChecking=no -i "${AWS_PRIVATE_KEY_PATH}" ${AWS_INSTANCE_URL} "IMAGE_URL=${AWS_ECR_REPO_URL} REGION=${AWS_INSTANCE_REGION} ${AWS_INSTANCE_DEPLOY_SCRIPT}"
+if [[ "$CIRCLE_BRANCH" == "master" ]] || [[ "$CIRCLE_BRANCH" == "main" ]]; then
+  echo -e "${COLOR}::::ssh and deploy::::${NC}"
+  ssh -o StrictHostKeyChecking=no -i "${AWS_PRIVATE_KEY_PATH}" ${AWS_INSTANCE_URL} "IMAGE_URL=${AWS_ECR_REPO_URL} REGION=${AWS_INSTANCE_REGION} ${AWS_INSTANCE_DEPLOY_SCRIPT}"
+fi
+
 
 echo -e "${COLOR}:::::::::Deployed by the CI:::::::::${NC}"
 
